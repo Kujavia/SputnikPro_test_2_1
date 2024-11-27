55c45c4f1fe338c14b245328b9927fd4afc6ffa8
SputnikPro <averina@sputnik.llc>
2024-11-27 19:09:28 +0300

    img4

--- src/js/main.js
+++ src/js/main.js
@@ -387,10 +387,10 @@ window.updateUserImage = function() {
   const userSelect = document.getElementById('header_top_user');
   const userImageDiv = document.getElementById('userImage');
   const images = {
-      option1: './public/images/question.jpg', // Путь к изображению для нового пользователя
-      option2: "/public/images/ZilevichElizaveta.jpg", // Путь к изображению Зилевич Елизаветы
+      option1: "../../images/users/question.jpg", //  для нового пользователя
+      option2: "../../images/users/ZilevichElizaveta.jpg", // Зилевич Елизаветы
       option3: '/images/user1.png', // Путь к изображению Исахановой Малики
-      option4: './public/images/TkachevOleg.jpg', // Путь к изображению Ткачёва Олега
+      option4: "../../images/users/TkachevOleg.jpg", 
       option5: 'https://github.com/Kujavia/SputnikPro_test_2_1/blob/gh-pages/images/TolokGalina.jpg', // Путь к изображению Шварёва Данила
       option6: 'https://github.com/Kujavia/SputnikPro_test_2_1/blob/master/public/images/TolokGalina.jpg' // Путь к изображению Толок Галина
   };
