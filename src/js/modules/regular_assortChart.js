a2e3ca1e58cb47c1e3215dca9cb40e2e072e48d7
SputnikPro <averina@sputnik.llc>
2024-11-27 17:36:27 +0300

    version2_1

---
+++ src/js/modules/regular_assortChart.js
@@ -0,0 +1,34 @@
+export default window.tb_regular_assort__chart = function(){
+    // const imageUrl = '/SputnikPro_test_2/public/images/chart_regAssort.png';
+    // document.getElementById('tb_regular_assort_chart').src = imageUrl;
+    const iframe = document.getElementById('tb_regular_assort_chart');
+    const imageUrl = '/SputnikPro_test_2/public/images/chart_regAssort.png'; // Замените на нужный URL
+
+    // Устанавливаем содержимое iframe
+    const doc = iframe.contentDocument || iframe.contentWindow.document;
+    doc.open();
+    doc.write(
+        `<html>
+            <head>
+                <style>
+                    body {
+                        margin: 0;
+                        display: flex;
+                        justify-content: start;
+                        align-items: start;
+                        height: 100vh;
+                    }
+                    img {
+                        min-width: 100%;
+                        min-height: 120%;
+                    }
+                </style>
+            </head>
+            <body>
+                <img src="${imageUrl}" alt="Image" />
+            </body>
+        </html>`
+    );
+    doc.close();
+  }
+  
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import {resolve} from "path";
+
+export default defineConfig({
+  base: '/SputnikPro_test_2/', 
+  build: {
+		outDir: 'dist',
+		rollupOptions: {
+			input: {
+				index: resolve(__dirname, "index.html"),
+				action: resolve(__dirname, "src/html/action.html"),
+				parameters: resolve(__dirname, "src/html/parameters.html"),
+				seasonality: resolve(__dirname, "src/html/seasonality.html"),
+				regular_assort: resolve(__dirname, "src/html/regular_assort.html"),
+				promo: resolve(__dirname, "src/html/promo.html"),
+				new_products: resolve(__dirname, "src/html/new_products.html"),
+			}
+		}
+	}
+});
